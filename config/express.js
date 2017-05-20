cdcd0dfe4adb1adda9e85e3cac6aacaf1e2d4753
Icaro Oliveira <Icaro Oliveira>
2017-05-20 17:14:16 -0300

    Configured the body-parser

--- config/express.js
+++ config/express.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var load = require('express-load');
+var bodyParser = require('body-parser');
 
 module.exports = function() {
     console.log('Initializing express');
@@ -8,6 +9,8 @@ module.exports = function() {
     console.log('Initializing ejs');
     app.set('view engine', 'ejs');
     app.set('views', './app/views');
+    
+    app.use(bodyParser.urlencoded({extended: true}));
     
     console.log('Loading required modules');
     load('routes', {cwd: 'app'})
